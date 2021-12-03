a67a899b4c8d3f429e2d961eff8f5ffc5283f019
xstefank <xstefank122@gmail.com>
2021-12-03 12:54:57 +0100

    Add slices example to cards

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)	
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
