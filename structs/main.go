c576db861db40825c504ba50df6ed794a0a9f2ef
xstefank <xstefank122@gmail.com>
2022-02-04 19:25:14 +0100

    Different way of working with struct

--- structs/main.go
+++ structs/main.go
@@ -8,9 +8,11 @@ type person struct {
 }
 
 func main() {
-	// alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	var alex person
 
-	fmt.Println(alex)
-	
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)	
+	fmt.Printf("%+v", alex)
 }
