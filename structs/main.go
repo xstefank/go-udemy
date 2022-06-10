e7d2b8284c0d350a759e680a2fd8574947edc5ac
xstefank <xstefank122@gmail.com>
2022-06-10 12:31:00 +0200

    Add last changes from the last module

--- structs/main.go
+++ structs/main.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 type contactInfo struct {
-	email string
+	email   string
 	zipCode int
 }
 
 type person struct {
 	firstName string
-	lastName string
+	lastName  string
 	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
-		lastName: "Party",
-		contactInfo: contactInfo {
-			email: "jim@gmail.com",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
