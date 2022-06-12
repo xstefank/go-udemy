c5a5009d3222ea8f74b82f3d08cc79d953318f21
xstefank <xstefank122@gmail.com>
2022-06-12 07:20:20 +0200

    Add custom logWriter

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -15,5 +17,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote", len(bs), "bytes")
+
+	return len(bs), nil
 }
